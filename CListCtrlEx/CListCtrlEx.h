df8689d794ff6101e69946866469106b5b745ceb
wth515812076 <515812076@qq.com>
2014-10-22 16:45:14 +0800

    sdasdad
    dadasdasd

---
+++ CListCtrlEx/CListCtrlEx.h
@@ -0,0 +1,32 @@
+
+// CListCtrlEx.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCListCtrlExApp:
+// 有关此类的实现，请参阅 CListCtrlEx.cpp
+//
+
+class CCListCtrlExApp : public CWinApp
+{
+public:
+	CCListCtrlExApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCListCtrlExApp theApp;
